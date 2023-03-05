129e37d269e10089a16db2fcdf5bea99579790de
Ali Hassan Ali <69222334+Ali-Hassan-Ali@users.noreply.github.com>
2023-03-06 01:10:36 +0200

    add

--- resources/views/front/layout/includes/header.blade.php
+++ resources/views/front/layout/includes/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header-top fixed-top">
     <div class="container d-flex justify-content-between">
       <a href="{{ route('change.lang', app()->getLocale() == 'ar' ? 'en': 'ar') }}">
-        {{ trans('front.lang_'. session('lang')) }}
+        {{ trans('front.lang_'.  app()->getLocale()) }}
       </a>
       <a href="{{ route('home') }}">{{ trans('front.home') }}</a>
     </div>
